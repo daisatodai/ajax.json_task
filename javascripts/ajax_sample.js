e70c4654abfb5eed4db483eb411ee7c7452eeed6
dai <sugerbig15@gmail.com>
2024-01-24 18:12:47 +0900

    更新しました

--- javascripts/ajax_sample.js
+++ javascripts/ajax_sample.js
@@ -1,24 +1,18 @@
 let number = 0;
-let data = []; // ajax.jsonから取得したデータを格納するための変数を追加
+let data = [];
 const button = document.getElementById('btn');
 const titleArea = document.getElementById("title");
 const contentArea = document.getElementById("content");
 const videoArea = document.getElementById("video");
 
 function getData() {
-  // return new Promise((resolve, reject) => {
     const request = new XMLHttpRequest();
     request.onreadystatechange = function() {
       if (request.readyState == 4) {
         if(request.status == 200) {
-          // try {
-            data = JSON.parse(request.responseText); // レスポンスをJSONとして解析
-            return data
-            // resolve(data);
-          // } catch (error) {
-          //   reject(error);
-          // }
-        // }
+            data = request.response;
+            changeVideo();
+        }
       }
     };
     request.open("GET", "ajax.json");
@@ -26,25 +20,16 @@ function getData() {
     request.send(null);
 }
 
-// ajax.jsonからデータを取得する処理を記述
-function changeVideo() {
-  button.addEventListener('click', function() {
-    // debugger
-    if (data == []){
+function changeVideo(){
+    if (!data.length){
       getData();
-      };
-    });
-  debugger
-    // getData().then(function(data) {
-    //   videoArea.innerHTML = data[number].url;
-    //   titleArea.innerHTML = data[number].title;
-    //   contentArea.innerHTML = data[number].content;
-    //   number == 2 ? number == 0 : number ++ ; 
-    // }).catch(function(error) {
-    //   console.error("Error fetching data:", error);
-    // });
+      return;
+      }
+      videoArea.setAttribute("src",data[number].url);
+      titleArea.innerHTML= data[number].title;
+      contentArea.innerHTML = data[number].content;
+      number == 2 ? (number = 0) : number ++ ; 
 }
-
-// ボタンがクリックされた際の処理を記述  
-// ajax.jsonからデータを取得していない場合のみ、getDataの処理を呼び出す
-window.onload = changeVideo;
+window.onload = function(){
+  button.addEventListener(`click`,changeVideo);
+};
